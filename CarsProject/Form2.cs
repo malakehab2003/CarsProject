a1e5d2e3b818f50cecbf274bcc36508974c8d42e
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-11 23:46:22 +0200

    add the form 2 to show the result of the search

---
+++ CarsProject/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsProject
+{
+
+    public partial class Form2 : Form
+    {
+        public Form2(List<string> data)
+        {
+            InitializeComponent();
+
+            listBox1.Items.AddRange(data.ToArray());
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
