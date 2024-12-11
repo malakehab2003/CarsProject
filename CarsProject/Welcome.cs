ceecba9f18c470b5de11b8fe182e840c4f190068
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-11 23:42:32 +0200

    add welcome form

---
+++ CarsProject/Welcome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsProject
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Signin form = new Signin();
+
+            this.Hide();
+
+            form.ShowDialog();
+
+            this.Close();
+        }
+    }
+}
