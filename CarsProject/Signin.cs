798d781232d9c4335fdd4802ca41d1bf33cc421f
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-12-11 23:43:59 +0200

    add Signin form to take the name of the user

---
+++ CarsProject/Signin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsProject
+{
+    public partial class Signin : Form
+    {
+        public Signin()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Signin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+
+            this.Hide();
+
+            form.ShowDialog();
+
+            this.Close();
+        }
+    }
+}
